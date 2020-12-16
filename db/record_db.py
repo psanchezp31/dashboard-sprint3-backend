42d426beed4300b2c31e5943eb60c3185edca23f
Paula Sanchez <paula.sanchez1031@gmail.com>
2020-12-16 14:54:16 -0500

    Agregando datos a la database ficticia

--- db/record_db.py
+++ db/record_db.py
@@ -20,48 +20,68 @@ class RecordInDB(BaseModel):
 database_records = Dict[int, RecordInDB]
 database_records = {
     1: RecordInDB(**{"id": 1,
-                     "categoria": "salary",
+                     "categoria": "Salary",
                      "tipo": "income",
                      "cantidad": 3000000,
                      "fecha_registro": datetime(2018, 12, 19).date()
                      }),
     2: RecordInDB(**{"id": 2,
-                     "categoria": "groceries",
+                     "categoria": "Food",
                      "tipo": "expense",
                      "cantidad": 150000,
                      "fecha_registro": datetime(2018, 12, 23).date()
                      }),
     3: RecordInDB(**{"id": 3,
-                     "categoria": "healthcare",
+                     "categoria": "Healthcare",
                      "tipo": "expense",
                      "cantidad": 45000,
                      "fecha_registro": datetime(2018, 12, 19).date()
                      }),
 
     4: RecordInDB(**{"id": 4,
-                     "categoria": "pet grooming",
+                     "categoria": "Pet",
                      "tipo": "expense",
                      "cantidad": 500000,
                      "fecha_registro": datetime(2018, 12, 25).date()
                      }),
 
     5: RecordInDB(**{"id": 5,
-                     "categoria": "mortage",
+                     "categoria": "Pet",
                      "tipo": "expense",
                      "cantidad": 150000,
                      "fecha_registro": datetime(2018, 12, 26).date()
+                     }),
+    6: RecordInDB(**{"id": 6,
+                     "categoria": "Food",
+                     "tipo": "expense",
+                     "cantidad": 55000,
+                     "fecha_registro": datetime(2019, 1, 2).date()
+                     }),
+    7: RecordInDB(**{"id": 7,
+                     "categoria": "Restaurants",
+                     "tipo": "expense",
+                     "cantidad": 88000,
+                     "fecha_registro": datetime(2019, 1, 3).date()
+                     }),
+    8: RecordInDB(**{"id": 8,
+                     "categoria": "Pet",
+                     "tipo": "expense",
+                     "cantidad": 35000,
+                     "fecha_registro": datetime(2019, 1, 6).date()
                      }),
 }
 
 
 ##############métodos para el POST y el GET###################
-generator = {"id":5}  # AUTOINCREMENTAL
+generator = {"id": 5}  # AUTOINCREMENTAL
+
 
 def save_record(record_in_db: RecordInDB):  # CREATE RECORD (POST)
     generator["id"] = generator["id"] + 1
     record_in_db.id = generator["id"]
     database_records[record_in_db.id] = record_in_db
     return record_in_db
+
 
 def get_record(id: int):  # READ RECORD (GET)
     if id in database_records.keys():
